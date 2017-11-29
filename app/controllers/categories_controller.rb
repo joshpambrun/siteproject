358a09ec78e9747fcb9a410b341fc04e34aa3f7e
Josh Pambrun <joshpambrun@gmail.com>
2017-11-29 21:59:33 +0000

    Added pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.where(name: params[:name])[0]    
-    @products = Product.where(category: @category).order("name").page(params[:page]).per(5)
+    @products = Product.where(category: @category).order("name").page(params[:page]).per(2)
   end
   
   def search
